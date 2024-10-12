73d883b8671f782a56f30aac3c1a9c72b1e85a7d
nick <ronilovo2@gmail.com>
2024-10-12 13:29:27 +0300

    Добавлен randomized тест

--- tests/lab2/test_vigenere.py
+++ tests/lab2/test_vigenere.py
@@ -1,3 +1,5 @@
+import random
+import string
 import unittest
 from src.lab2.vigenre import encrypt_vigenere, decrypt_vigenere
 
@@ -6,4 +8,11 @@ class VigenereTestCase(unittest.TestCase):
     def test_encrypt(self):
         self.assertEqual(encrypt_vigenere("GEEKSFORGEEKS", "AYUSH"), "GCYCZFMLYLEIM")
     def test_decrypt(self):
-        self.assertEqual(decrypt_vigenere("GCYCZFMLYLEIM", "AYUSH"), "GEEKSFORGEEKS")
+        self.assertEqual(decrypt_vigenere("GCYCZFMLYLEIM", "AYUSH"), "GEEKSFORGEEKS") \
+
+    def test_randomized(self):
+        kwlen = random.randint(4, 24)
+        keyword = ''.join(random.choice(string.ascii_letters) for _ in range(kwlen))
+        plaintext = ''.join(random.choice(string.ascii_letters + ' -,') for _ in range(64))
+        ciphertext = encrypt_vigenere(plaintext, keyword)
+        self.assertEqual(plaintext, decrypt_vigenere(ciphertext, keyword))
