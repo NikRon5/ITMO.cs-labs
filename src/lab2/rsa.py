cc7a32d0ec2678be17383d327dfd4972bcc4cdde
nick <ronilovo2@gmail.com>
2024-10-12 14:08:26 +0300

    Исправление фунции для проверки простого числа

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -12,6 +12,7 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
+    if n <= 1: return False
     for i in range(2, int(n**0.5+1)):
         if n % i == 0: return False
     return True
