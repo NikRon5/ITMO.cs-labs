428639644960ac604020e3cf403824c4c34beaae
Nick <ronilovo2@gmail.com>
2024-12-18 19:30:57 +0300

    Lab 5

--- src/lab5/orders_manager.py
+++ src/lab5/orders_manager.py
@@ -17,13 +17,13 @@ class OrdersManager:
 
 
     def write_valid_orders(self) -> None:
-        to_write = self.__get_valid_orders_output()
+        to_write = self.get_valid_orders_output()
         f = open(self.__order_county_path, "w", encoding="utf-8")
         f.write("\n".join(to_write))
         f.close()
 
     def write_non_valid_orders(self) -> None:
-        to_write = self.__get_non_valid_orders_output()
+        to_write = self.get_non_valid_orders_output()
         f = open(self.__non_valid_orders_path, "w", encoding="utf-8")
         f.write(to_write)
         f.close()
